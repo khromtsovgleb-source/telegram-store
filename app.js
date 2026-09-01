018499fd822a7c606eeb845cc7f3c1271f6b4bd2
khromtsovgleb-source <khromtsovgleb@gmail.com>
2026-09-01 18:25:26 +0300

    app.js

--- app.js
+++ app.js
@@ -6,35 +6,35 @@ tg.expand();
 const user = tg.initDataUnsafe.user;
 
 document.getElementById("username").innerText =
-user
-? `Привет, ${user.first_name}! 👋`
-: "Добро пожаловать!";
+    user
+        ? `Привет, ${user.first_name}! 👋`
+        : "Добро пожаловать!";
 
 const products = [
 
-{
-id:1,
-name:"Starter",
-price:2,
-desc:"Тестовый аккаунт с моментальной выдачей.",
-img:"https://picsum.photos/600/400?random=1"
-},
+    {
+        id: 1,
+        name: "START",
+        price: 2,
+        desc: "Тестовый аккаунт с моментальной выдачей.",
+        img: "resized-image_part_001.jpg"
+    },
 
-{
-id:2,
-name:"Premium",
-price:3,
-desc:"Расширенный тестовый аккаунт.",
-img:"https://picsum.photos/600/400?random=2"
-},
+    {
+        id: 2,
+        name: "VIP",
+        price: 2,
+        desc: "VIP тестовый аккаунт.",
+        img: "resized-image_part_003.jpg"
+    },
 
-{
-id:3,
-name:"VIP",
-price:2,
-desc:"VIP тестовый аккаунт.",
-img:"https://picsum.photos/600/400?random=3"
-}
+    {
+        id: 3,
+        name: "PREMIUM",
+        price: 3,
+        desc: "Расширенный тестовый аккаунт.",
+        img: "resized-image_part_002.jpg"
+    }
 
 ];
 
@@ -42,50 +42,50 @@ const list = document.getElementById("products");
 
 products.forEach(product => {
 
-list.innerHTML += `
+    list.innerHTML += `
 
-<div class="card">
+        <div class="card">
 
-<img src="${product.img}">
+            <img src="${product.img}" alt="${product.name}">
 
-<div class="content">
+            <div class="content">
 
-<div class="title">${product.name}</div>
+                <div class="title">${product.name}</div>
 
-<div class="desc">${product.desc}</div>
+                <div class="desc">${product.desc}</div>
 
-<div class="bottom">
+                <div class="bottom">
 
-<div class="price">${product.price} ⭐</div>
+                    <div class="price">${product.price} ⭐</div>
 
-<button
-class="buy"
-onclick="buy(${product.id})">
+                    <button
+                        class="buy"
+                        onclick="buy(${product.id})">
 
-Купить
+                        Купить
 
-</button>
+                    </button>
 
-</div>
+                </div>
 
-</div>
+            </div>
 
-</div>
+        </div>
 
-`;
+    `;
 
 });
 
-function buy(id){
+function buy(id) {
 
-tg.sendData(JSON.stringify({
+    tg.sendData(JSON.stringify({
 
-action:"buy",
+        action: "buy",
 
-product:id
+        product: id
 
-}));
+    }));
 
-tg.close();
+    tg.close();
 
 }
